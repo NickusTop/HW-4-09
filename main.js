2d865bdeea45c86b8d30bccb7fb5346ef2bfdb26
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-07-30 11:30:08 +0300

    HW

---
+++ main.js
@@ -0,0 +1,18 @@
+const inputName = document.querySelector('.input-name')
+const inputSurName = document.querySelector('.input-surname')
+const inputEmail = document.querySelector('input-email')
+const inputNumber = document.querySelector('input-number')
+const inputPassword = document.querySelector('input-password')
+const buttonSave = document.querySelector('button-save')
+
+buttonSave.addEventListener('click', buttonSaveData)
+
+function buttonSaveData() {
+    const inputNameValue = inputName.value.trim()
+    const inputSurNameValue = inputSurName.value.trim()
+    const inputEmailValue = inputEmail.value.trim()
+    const inputNumberValue = inputEmail.value.trim()
+    const inputPasswordValue = inputPassword.value.trim()
+    
+    
+}
